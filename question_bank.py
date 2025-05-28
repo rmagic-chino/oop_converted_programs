8bddb00577d5ae9503c750ef7dd3b0d535b7c101
rmagic-chino <raremagic01@gmail.com>
2025-05-29 03:50:00 +0800

    set parameters for answers

--- question_bank.py
+++ question_bank.py
@@ -1,3 +1,13 @@
 class QuestionBank:
     def __init__(self):
-        self.questions = []   
+        self.questions = []
+
+    def add_question(
+            self,
+            question_text,
+            option_a,
+            option_b,
+            option_c,
+            option_d,
+            correct_option,
+    ):
