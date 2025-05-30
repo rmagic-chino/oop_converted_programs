c6f133910be452d17b633bbc640d978f5af467a3
Carlos De Guzman <raremagic01@gmail.com>
2025-05-31 01:58:02 +0800

    show end message if no more questions left

--- quiz_loader/quiz_logic.py
+++ quiz_loader/quiz_logic.py
@@ -8,4 +8,8 @@ class QuizLogic:
         self.on_question_change = on_question_change
         self.on_quiz_end = on_quiz_end
         
-    
+    def next_question(self):
+        if not self.questions:
+            messagebox.showinfo("Quiz Ended", "No more questions available.")
+            self.on_quiz_end()
+            return
