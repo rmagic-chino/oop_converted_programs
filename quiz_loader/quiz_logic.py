57dd706f778fcb9de8a147daaea8c3a3add7e043
Carlos De Guzman <raremagic01@gmail.com>
2025-05-31 02:00:20 +0800

    show message if answer is correct or incorrect

--- quiz_loader/quiz_logic.py
+++ quiz_loader/quiz_logic.py
@@ -13,3 +13,16 @@ class QuizLogic:
             messagebox.showinfo("Quiz Ended", "No more questions available.")
             self.on_quiz_end()
             return
+        
+        self.current_question = random.choice(self.questions)
+        self.questions.remove(self.current_question)
+        self.on_question_change(self.current_question)
+        
+    def check_answer(self, selected_option):
+        correct = self.current_question['correct']
+        if selected_option == correct:
+            messagebox.showinfo("Correct!", "Tama boss!")
+        else:
+            messagebox.showerror("Incorrect", f"Mali boss! Ang tamang sagot ay {correct}.")
+        self.next_question()
+        
